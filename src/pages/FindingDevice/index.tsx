7edfbfa619bda255ac69f2f230ebc068925070c4
Abolfazl Mikaniki <abolfazlmikaniki6@gmail.com>
2024-10-12 18:53:31 +0330

    complete FindingDevice page

--- src/pages/FindingDevice/index.tsx
+++ src/pages/FindingDevice/index.tsx
@@ -1,8 +1,10 @@
+import Devices from '../../components/common/Devices'
+import DeviceDetection from '../../components/DeviceDetection'
 import Layout from '../../layouts/Main'
 
 const FindingDevice = () => {
   return (
-    <Layout/>
+    <Layout children1={<DeviceDetection/>} children2={<Devices/>}/>
   )
 }
 
