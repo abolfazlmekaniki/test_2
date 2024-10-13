1e65db988fea00ea34bbce9b6f7ec8b75a463038
Abolfazl Mikaniki <abolfazlmikaniki6@gmail.com>
2024-10-13 15:44:26 +0330

    edit

--- src/layouts/Main/index.tsx
+++ src/layouts/Main/index.tsx
@@ -8,7 +8,7 @@ interface props{
 }
 const Layout:React.FC<props> = ({children1,children2,text="or",hr=true}) => {
   return (
-        <main className="bg-gradient-to-b  min-h-screen py-14 flex flex-col  space-y-10 overflow-x-hidden justify-start items-center w-screen  bg-bg_pink drop-shadow-2xl">
+        <main className="bg-gradient-to-b  min-h-screen py-10 flex flex-col  space-y-10 overflow-x-hidden justify-start items-center w-screen  bg-bg_pink drop-shadow-2xl">
             {children1}
             {hr && <div className="flex space-x-2 justify-between items-center">
                 <div className="w-20 h-[1px] bg-hr_bg_color"></div>
